ff67da8fd0ffad449b7566c121f96f549b55ee42
juanjuan <17710373802@163.com>
2017-06-30 10:42:24 +0800

    修改项目

--- cloud_config_client/src/main/java/com/example/cloud_config_client/CloudConfigClientApplication.java
+++ cloud_config_client/src/main/java/com/example/cloud_config_client/CloudConfigClientApplication.java
@@ -1,10 +1,24 @@
 package com.example.cloud_config_client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@Configuration
 public class CloudConfigClientApplication {
+	@Value("${label}")
+	private String label;
+
+	@RequestMapping("/restart")
+	public String hello() {
+		return label+"hello";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfigClientApplication.class, args);

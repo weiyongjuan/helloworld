9b5da96168d86bc2385e50790052968a5e8b5640
weiyongjuan <weiyj9600@163.com>
2017-07-02 22:15:02 +0800

    修改配置

--- cloud_config_client2/src/main/java/com/example/cloud_config_client2/CloudConfigClient2Application.java
+++ cloud_config_client2/src/main/java/com/example/cloud_config_client2/CloudConfigClient2Application.java
@@ -3,7 +3,6 @@ package com.example.cloud_config_client2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @Configuration
 @RestController
-@EnableZuulProxy
 public class CloudConfigClient2Application {
 	
 	@Value("${name}")
--- spring_cloud_zuul/src/main/java/com/example/spring_cloud_zuul/SpringCloudZuulApplication.java
+++ spring_cloud_zuul/src/main/java/com/example/spring_cloud_zuul/SpringCloudZuulApplication.java
@@ -2,12 +2,12 @@ package com.example.spring_cloud_zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class SpringCloudZuulApplication {
 
 	public static void main(String[] args) {

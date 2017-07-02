5344bb621b7978f6de1ad6993997aa1843dc5b5c
weiyongjuan <weiyj9600@163.com>
2017-07-02 21:57:14 +0800

    增加项目

--- cloud_config_client/src/main/java/com/example/cloud_config_client/CloudConfigClientApplication.java
+++ cloud_config_client/src/main/java/com/example/cloud_config_client/CloudConfigClientApplication.java
@@ -3,6 +3,7 @@ package com.example.cloud_config_client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @Configuration
+@EnableZuulProxy
 public class CloudConfigClientApplication {
 	@Value("${name}")
 	private String name;
 	
-	@RequestMapping("/restart")
+	@RequestMapping("start1")
 	public String hello() {
 		return name+" hello World";
 	}
--- cloud_config_client2/src/main/java/com/example/cloud_config_client2/CloudConfigClient2Application.java
+++ cloud_config_client2/src/main/java/com/example/cloud_config_client2/CloudConfigClient2Application.java
@@ -3,6 +3,7 @@ package com.example.cloud_config_client2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @Configuration
 @RestController
+@EnableZuulProxy
 public class CloudConfigClient2Application {
 	
 	@Value("${name}")
 	private String name;
 	
-	@RequestMapping("/restart")
+	@RequestMapping("/start2")
 	public String hello() {
 		return name+" hello World";
 	}

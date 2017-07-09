633dec4e039b6ef1104bb8ca570307a37a71c2ae
weiyongjuan <weiyj9600@163.com>
2017-07-09 17:31:43 +0800

    《增加配置》

--- cloud_config_client/src/main/java/com/example/cloud_config_client/CloudConfigClientApplication.java
+++ cloud_config_client/src/main/java/com/example/cloud_config_client/CloudConfigClientApplication.java
@@ -1,5 +1,6 @@
 package com.example.cloud_config_client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,15 +8,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.cloud_config_client.entity.sysUser;
+import com.example.cloud_config_client.server.userServer;
+
 @SpringBootApplication
 @RestController
 @Configuration
 public class CloudConfigClientApplication {
 	@Value("${name}")
 	private String name;
+	@Autowired
+	private userServer sysUserServer;
 	
 	@RequestMapping("start1")
 	public String hello() {
+		sysUser user = sysUserServer.findAll();
+		System.out.println(user.toString());
 		return name+" hello World";
 	}
 

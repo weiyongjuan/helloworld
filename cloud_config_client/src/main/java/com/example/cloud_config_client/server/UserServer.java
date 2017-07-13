2a40b1ad75418d62abfa2c563ab7f8ae93683d9f
juanjuan <17710373802@163.com>
2017-07-13 14:22:36 +0800

    《修改配置文件》

--- cloud_config_client/src/main/java/com/example/cloud_config_client/server/UserServer.java
+++ cloud_config_client/src/main/java/com/example/cloud_config_client/server/UserServer.java
@@ -3,12 +3,12 @@ package com.example.cloud_config_client.server;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.example.cloud_config_client.dao.UserMapper;
 import com.example.cloud_config_client.entity.sysUser;
 
-@Component
+@Service
 public class UserServer {
 	@Autowired
 	private UserMapper userMapper;

1aa3e0cdee03fc1855d0b9b7af6b5871c35e22e3
juanjuan <17710373802@163.com>
2017-07-13 15:32:30 +0800

    《修改路劲》

--- cloud_config_client/src/main/java/com/example/cloud_config_client/server/UserServer.java
+++ cloud_config_client/src/main/java/com/example/cloud_config_client/server/UserServer.java
@@ -3,15 +3,18 @@ package com.example.cloud_config_client.server;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
 
 import com.example.cloud_config_client.dao.UserMapper;
 import com.example.cloud_config_client.entity.sysUser;
 
-@Component
+@Service
+@ComponentScan(basePackages="com.example.cloud_config_client.dao")
 public class UserServer {
 	
 	@Autowired
+	
 	private UserMapper userMapper;
 	
 	public List<sysUser> findAll(){

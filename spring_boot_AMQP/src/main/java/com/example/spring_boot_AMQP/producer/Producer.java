f12d12bc62b0901e800c47803b073737c38e4e68
juanjuan <17710373802@163.com>
2017-07-14 17:04:03 +0800

    修改mq

--- spring_boot_AMQP/src/main/java/com/example/spring_boot_AMQP/producer/Producer.java
+++ spring_boot_AMQP/src/main/java/com/example/spring_boot_AMQP/producer/Producer.java
@@ -4,17 +4,19 @@ import java.util.Date;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 发送消息
  * @author Dell
  *
  */
-@Component
+@RestController
 public class Producer {
 	@Autowired
 	private AmqpTemplate amqpTemplate;
-	
+	@RequestMapping(value="/helo")
 	public void sender(){
 		String context = "hello"+new Date();
 		System.out.println(context);

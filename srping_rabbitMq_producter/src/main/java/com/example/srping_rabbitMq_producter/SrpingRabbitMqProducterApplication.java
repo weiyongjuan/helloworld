96dd9433cc8bca393b3d8840bc0278999e1c2c0c
juanjuan <17710373802@163.com>
2017-08-01 18:24:20 +0800

    新增项目

--- srping_rabbitMq_producter/src/main/java/com/example/srping_rabbitMq_producter/SrpingRabbitMqProducterApplication.java
+++ srping_rabbitMq_producter/src/main/java/com/example/srping_rabbitMq_producter/SrpingRabbitMqProducterApplication.java
@@ -2,7 +2,11 @@ package com.example.srping_rabbitMq_producter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * http://blog.720ui.com/2017/rabbitmq_action_email/
+ * @author Dell
+ *
+ */
 @SpringBootApplication
 public class SrpingRabbitMqProducterApplication {
 

b817ef0b65da454f5a88ac9717c4b14bcd2e80ce
juanjuan <17710373802@163.com>
2017-08-18 13:55:40 +0800

    修改项目

--- spring_boot_AMQP/src/main/java/com/example/spring_boot_AMQP/SpringBootAmqpApplication.java
+++ spring_boot_AMQP/src/main/java/com/example/spring_boot_AMQP/SpringBootAmqpApplication.java
@@ -2,7 +2,12 @@ package com.example.spring_boot_AMQP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * http://blog.csdn.net/column/details/rabbitmq.html
+ * RabbitMQ从入门到精通
+ * @author Dell
+ *
+ */
 @SpringBootApplication
 public class SpringBootAmqpApplication {
 
